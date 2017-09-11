f0278dd04cacab6301a085b5ef9a823514d276b4
Maohua Xie <maohuaxie@gmail.com>
2017-09-11 19:33:37 -0400

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -1,10 +1,13 @@
-    
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep 11 19:05:10 2017
 
 @author: maohuaxie
 """
+#On Windows os.rename won't replace the destination file if it exists. 
+#You have to remove it first. You can catch the error and try again after removing the file:
+#On Linux os.rename will replace the desitination file if it exists. So, I highly recommend you use Linux platform.
+
 import os
 os.chdir("D:/bioinformatics/copyvcf3")
 for f in os.listdir():
